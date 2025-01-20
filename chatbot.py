895af615bdf093745ce368d7cd69721d6b3ebbb3
Debasish Paul <116668530+debasishpaul999@users.noreply.github.com>
2025-01-20 10:08:04 +0530

    added html css

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import ollama
 
-st.title("💬 llama2 (7B) Chatbot")
+st.title("Clara")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
